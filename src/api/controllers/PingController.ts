d1df7a2db5fe34bb1237e0093172423975f15e44
Wale K. Santos <eighteen.eighty.one@outlook.com>
2024-04-26 02:51:19 +0100

    add ping endpoint to keep demo server alive

--- src/api/controllers/PingController.ts
+++ src/api/controllers/PingController.ts
@@ -1,4 +1,5 @@
 import moment from "moment";
+import momentTz from "moment-timezone";
 import { Get, JsonController, Post } from "routing-controllers";
 import { OpenAPI } from "routing-controllers-openapi";
 
@@ -7,16 +8,16 @@ import { SuccessResponse } from "../responses";
 
 
 @JsonController("/ping")
-export default class HealthController {
+export default class PingController {
     @OpenAPI({ summary: "Ping" })
     @Post()
     public async healthCheckPost( ): Promise<IResponse> {
-        return Promise.resolve(new SuccessResponse(`Application pinged at ${moment().format("MMMM Do YYYY, h:mm:ss a")}`));
+        return Promise.resolve(new SuccessResponse(`Application pinged at ${momentTz.tz(moment(), "Africa/Lagos").format("MMMM Do YYYY, h:mm:ss a")}`));
     }
 
     @OpenAPI({ summary: "Ping" })
     @Get()
     public async healthCheckGet( ): Promise<IResponse> {
-        return Promise.resolve(new SuccessResponse(`Application pinged at ${moment().format("MMMM Do YYYY, h:mm:ss a")}`));
+        return Promise.resolve(new SuccessResponse(`Application pinged at ${momentTz.tz(moment(), "Africa/Lagos").format("MMMM Do YYYY, h:mm:ss a")}`));
     }
 }
